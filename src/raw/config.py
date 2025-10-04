c6c0652b9fe459415baadda1bb7e970e20ef80fd
Dvodnenko <thekovalskii@gmail.com>
2025-10-04 18:29:51 +0300

    feat: add config file

---
+++ src/raw/config.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+import json
+
+from .domain import Config, CoreSettings
+
+
+CONFIG_FILE_PATH = Path.home() / ".config" / "raw" / "config.json"
+
+
+def load_config() -> Config:
+    with open(CONFIG_FILE_PATH, "r") as file:
+        data: dict = json.load(file)
+
+    core = CoreSettings(rootgroup=data.get("rootgroup"))
+    config = Config(core=core)
+    return config
