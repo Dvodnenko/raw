3551c47b6299be0880d85e9366e21d051b975dce
Dvodnenko <thekovalskii@gmail.com>
2025-10-12 20:52:00 +0300

    feat: add converting methods

--- src/raw/rawd/entities/reqres/entity.py
+++ src/raw/rawd/entities/reqres/entity.py
@@ -1,3 +1,5 @@
+import dataclasses
+import json
 from dataclasses import dataclass
 from typing import Generic, Optional, TypeVar
 
@@ -13,6 +15,13 @@ class Response(Generic[T]):
     @property
     def success(self) -> bool:
         return self.status_code == 0
+    
+    def to_bytes(self) -> "Response":
+        return json.dumps(dataclasses.asdict(self)).encode("utf-8")
+    
+    @staticmethod
+    def from_bytes(data: bytes) -> "Response":
+        return Response(**json.loads(data.decode("utf-8")))
 
 
 @dataclass
@@ -21,3 +30,10 @@ class Request(Generic[T]):
     command: str
     args: list[str]
     options: dict[str, str]
+
+    def to_bytes(self) -> "Request":
+        return json.dumps(dataclasses.asdict(self)).encode("utf-8")
+    
+    @staticmethod
+    def from_bytes(data: bytes) -> "Request":
+        return Request(**json.loads(data.decode("utf-8")))
