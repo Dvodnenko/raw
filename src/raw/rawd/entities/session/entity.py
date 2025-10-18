a3a4e533b220bfb2948f0cbf8b697dee13b054a6
Dvodnenko <thekovalskii@gmail.com>
2025-10-18 18:23:07 +0300

    refactor: remove breaks field in session model

--- src/raw/rawd/entities/session/entity.py
+++ src/raw/rawd/entities/session/entity.py
@@ -16,26 +16,15 @@ class Session(Entity):
     title: str = field(default_factory=generate_title)
     summary: str | None = field(default=None, kw_only=True)
     end: datetime | None = field(default=None, kw_only=True)
-    _breaks: list[timedelta] = field(
-        default_factory=lambda : [],
-        kw_only=True
-    )
 
     @property
     def is_active(self) -> bool:
         "Wether the Session is active"
         return self.end is None
-    
-    @property
-    def breaks(self) -> timedelta:
-        res = timedelta(0, 0, 0, 0, 0, 0, 0)
-        for td in self._breaks:
-            res += td
-        return res
 
     @property
     def total(self) -> timedelta:
         if self.is_active:
             raise SessionIsActiveError('Total time is not accessible because the session is still active')
-        res = self.end - self.start - self.breaks
+        res = self.end - self.start
         return res
