99bfdef6f935897a3fa5b9cfe19da619f23261b3
Dvodnenko <thekovalskii@gmail.com>
2025-11-03 17:45:36 +0200

    feat: add plist file generation

---
+++ src/raw/cli/icmds/darwin/general.py
@@ -0,0 +1,42 @@
+import shutil
+import json
+import plistlib
+
+from ...constants import CONFIG_PATH, DEFAULT_CONFIG, generate_plist, PLIST_PATH
+from ....config import load_config
+
+
+def raw_init(args, kwargs, flags):
+    
+    ## Config
+
+    force = "F" in flags
+
+    if not CONFIG_PATH.exists():
+        CONFIG_PATH.parent.mkdir(parents=True, exist_ok=True)
+        CONFIG_PATH.touch(exist_ok=True)
+        with open(CONFIG_PATH, "w") as file:
+            json.dump(DEFAULT_CONFIG, file, indent=4)
+    elif force:
+        with open(CONFIG_PATH, "w") as file:
+            json.dump(DEFAULT_CONFIG, file, indent=4)
+    else:
+        yield "You already have a non-default config file", 1
+        
+
+    ## Plist file (for macOS)
+
+    config = load_config()
+    plist_content = generate_plist(config.get("daemon_bin_path", 
+                                              shutil.which("rawd")))
+    if not PLIST_PATH.exists():
+        PLIST_PATH.touch(exist_ok=True)
+        with open(PLIST_PATH, "wb") as file:
+            plistlib.dump(plist_content, file)
+        return
+    elif force:
+        with open(PLIST_PATH, "wb") as file:
+            plistlib.dump(plist_content, file)
+        return
+    else:
+        yield "You already have a non-default plist file", 1
