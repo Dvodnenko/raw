5ad8f89dc995f0457a4d8b4b7b569e0c291e5156
Dvodnenko <thekovalskii@gmail.com>
2025-12-17 18:56:22 +0200

    add entity creation method

--- src/raw/daemon/repository.py
+++ src/raw/daemon/repository.py
@@ -9,6 +9,32 @@ from .database.mappings import (
 )
 
 
+def create(conn: Connection, table: str, **kwargs):
+    columns = {*kwargs.keys()}.difference("id")
+    entity_values = {c: kwargs[c] 
+        for c in columns.intersection(TABLES_COLUMNS["entities"].keys())}
+    this_values = {c: kwargs[c] 
+        for c in columns.intersection(TABLES_COLUMNS[table].keys())}
+    links: list[int] | None = kwargs.get("links")
+    
+    stmt1 = (
+        insert(entities_table)
+        .values(**entity_values)
+        .returning(entities_table.c.id)
+    )
+    entity_part = conn.execute(stmt1).one()
+
+    this_values["id"] = entity_part.id
+    stmt2 = (
+        insert(TABLES[table])
+        .values(**this_values)
+    )
+    conn.execute(stmt2)
+    if links:
+        next(link_entity(conn, entity_part.id, links))
+    yield
+
+
 def link_entity(conn: Connection, id: int, ids: Sequence[int]):
     stmt = insert(links_table)
     conn.execute(
