727c53fbb8810d154eaf26b865f45ff47590c366
Dvodnenko <thekovalskii@gmail.com>
2025-12-21 17:54:33 +0200

    add method for attaching links to entities

--- src/raw/daemon/repository/assemblers.py
+++ src/raw/daemon/repository/assemblers.py
@@ -20,3 +20,11 @@ def group_links(rows):
         grouped[from_id].append(build_entity(**row))
 
     return grouped
+
+def attach_links(entity_rows, link_rows):
+    links_by_entity = group_links(link_rows)
+
+    for row in entity_rows:
+        entity = build_entity(**row)
+        entity.links = links_by_entity.get(entity.id, [])
+        yield entity
