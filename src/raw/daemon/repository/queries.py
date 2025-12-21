9edef031cf5e0e69716656893f195b0cfcfe86e7
Dvodnenko <thekovalskii@gmail.com>
2025-12-21 17:50:05 +0200

    add method for links fetching

--- src/raw/daemon/repository/queries.py
+++ src/raw/daemon/repository/queries.py
@@ -2,7 +2,7 @@ from sqlalchemy import Connection, select
 
 from ..database.mappings import (
     entities_table, sessions_table, 
-    tasks_table, notes_table
+    tasks_table, notes_table, links_table
 )
 
 
@@ -18,7 +18,6 @@ def fetch_entities_batch(conn: Connection, limit: int, offset: int):
         .offset(offset)
     )
     return conn.execute(stmt).mappings().all()
-
 
 def enrich_entities(conn: Connection, ids: list[int]):
     stmt = (
@@ -41,6 +40,38 @@ def enrich_entities(conn: Connection, ids: list[int]):
         .outerjoin(tasks_table, tasks_table.c.id == entities_table.c.id)
         .outerjoin(notes_table, notes_table.c.id == entities_table.c.id)
         .order_by(entities_table.c.id)
+    )
+
+    return conn.execute(stmt).mappings().all()
+
+def fetch_outgoing_links(conn: Connection, from_ids: list[int]):
+    stmt = (
+        select(
+            entities_table.c.id,
+            entities_table.c.type,
+            entities_table.c.parent_id,
+            entities_table.c.title,
+            entities_table.c.description,
+            entities_table.c.styles,
+            entities_table.c.icon,
+
+            links_table.c.from_id,
+
+            sessions_table.c.start,
+            sessions_table.c.end,
+            sessions_table.c.summary,
+
+            tasks_table.c.deadline,
+            tasks_table.c.status,
+
+            notes_table.c.content,
+        )
+        .where(links_table.c.from_id.in_(from_ids))
+        .join(entities_table, entities_table.c.id == links_table.c.to_id)
+        .outerjoin(sessions_table, sessions_table.c.id == entities_table.c.id)
+        .outerjoin(tasks_table, tasks_table.c.id == entities_table.c.id)
+        .outerjoin(notes_table, notes_table.c.id == entities_table.c.id)
+        .order_by(links_table.c.from_id)
     )
 
     return conn.execute(stmt).mappings().all()
