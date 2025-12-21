d6bf3be9c82c70bdb9fe2e56c5cbcd026d86e42a
Dvodnenko <thekovalskii@gmail.com>
2025-12-21 17:53:17 +0200

    add method for links grouping

--- src/raw/daemon/repository/assemblers.py
+++ src/raw/daemon/repository/assemblers.py
@@ -1,4 +1,5 @@
 import inspect
+from collections import defaultdict
 
 from ..entities import ENTITIES
 
@@ -7,3 +8,15 @@ def build_entity(**data):
     cls = ENTITIES[data.pop("type")]
     params = inspect.signature(cls).parameters
     return cls(**{k:data.get(k) for k in params})
+
+def group_links(rows):
+    grouped = defaultdict(list)
+
+    for row in rows:
+        from_id = row["from_id"]
+        row = dict(row)
+        row.pop("from_id")
+
+        grouped[from_id].append(build_entity(**row))
+
+    return grouped
