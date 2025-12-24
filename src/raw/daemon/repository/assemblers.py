365b3865f99fcb77237610f047767e2f2c551a82
Dvodnenko <thekovalskii@gmail.com>
2025-12-24 20:21:40 +0200

    add filter resolver
    iterates through all filters and determines which tables they should be applied to

--- src/raw/daemon/repository/assemblers.py
+++ src/raw/daemon/repository/assemblers.py
@@ -1,6 +1,8 @@
 import inspect
 from collections import defaultdict
+from typing import Any
 
+from ..database.mappings import COLUMN_TO_TABLE
 from ..entities import ENTITIES
 
 
@@ -28,3 +30,19 @@ def attach_links(entity_rows, link_rows):
         entity = build_entity(**row)
         entity.links = links_by_entity.get(entity.id, [])
         yield entity
+
+def resolve_tables_to_filter(
+    filters: dict[str, tuple[Any]]
+):
+    table_to_filter_map: dict[str, dict[str, tuple[Any]]] = {}
+    for key, value in filters.items():
+        if "__" in key: # then it's a complex expression
+            table = COLUMN_TO_TABLE[key[0:key.index("__")]]
+        else:
+            table = COLUMN_TO_TABLE[key]
+
+        if table_to_filter_map.get(table.name):
+            table_to_filter_map[table.name].update({key: value})
+        else:
+            table_to_filter_map[table.name] = {key: value}
+    return table_to_filter_map
