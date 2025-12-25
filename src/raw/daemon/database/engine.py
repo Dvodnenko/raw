602e7209bc5b97de5182d4a4247cd09b495b42b2
Dvodnenko <thekovalskii@gmail.com>
2025-12-25 19:19:40 +0200

    enable foreign keys using pragma
    cascades don't work without it

--- src/raw/daemon/database/engine.py
+++ src/raw/daemon/database/engine.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event, Engine
 
 from ...common import config_, drill
 
@@ -7,3 +7,9 @@ engine = create_engine(
     url="sqlite:///" + drill(config_, ["core", "db_path"], raise_=True),
     echo=drill(config_, ["core", "echo"], default=False)
 )
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
