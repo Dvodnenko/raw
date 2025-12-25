40640079da57c8b6eed1464c8ad5210459109aad
Dvodnenko <thekovalskii@gmail.com>
2025-12-25 19:17:14 +0200

    add deletion method

--- src/raw/daemon/repository/dml.py
+++ src/raw/daemon/repository/dml.py
@@ -113,3 +113,19 @@ def update(conn: Connection, id: int, **kwargs):
         link_entity(conn, this_row["id"], links)
 
     return build_entity(**entity_row, **this_row)
+
+def delete(conn: Connection, id: int):
+    stmt1 = (
+        sa_delete(entities_table)
+        .where(entities_table.c.id == id)
+        .returning(
+            entities_table.c.title,
+            entities_table.c.type
+        )
+    )
+    entity_row = conn.execute(stmt1).one()
+    table = TABLES[entity_row.type+"s"]
+    stmt2 = sa_delete(table).where(table.c.id == id)
+    conn.execute(stmt2)
+
+    return None
