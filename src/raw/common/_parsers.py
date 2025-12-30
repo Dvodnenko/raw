2efab4094cb88d53904b3793f3695cadf3579c85
Dvodnenko <thekovalskii@gmail.com>
2025-12-30 16:34:00 +0200

    remove equality sign parsing (in cli options)

--- src/raw/common/_parsers.py
+++ src/raw/common/_parsers.py
@@ -12,11 +12,7 @@ def afk_parser(argv: list[str]) -> tuple:
     while i < len(argv):
         token = argv[i]
 
-        if token.startswith("--") and "=" in token:
-            key, value = token[2:].split("=", 1)
-            kwargs[key] = value
-
-        elif token.startswith("--"):
+        if token.startswith("--"):
             key = token[2:]
             if i + 1 < len(argv) and not argv[i + 1].startswith("-"):
                 kwargs[key] = argv[i + 1]
@@ -44,14 +40,7 @@ def sql_parser(argv: list[str]) -> dict:
     while i < len(argv):
         token = argv[i]
 
-        if token.startswith("--") and "=" in token:
-            key, value = token[2:].split("=", 1)
-            if filters.get(key):
-                filters[key].append(value)
-            else:
-                filters[key] = [value]
-
-        elif token.startswith("--"):
+        if token.startswith("--"):
             key = token[2:]
             if i + 1 < len(argv) and not argv[i + 1].startswith("-"):
                 if filters.get(key):
