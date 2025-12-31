f784bae3a84d635b5dd681bf3a0deb115782d4b5
Dvodnenko <thekovalskii@gmail.com>
2025-12-31 10:49:36 +0200

    add needed domain mappings-helpers

--- src/raw/daemon/domain/utils.py
+++ src/raw/daemon/domain/utils.py
@@ -1,4 +1,5 @@
 import inspect
+from dataclasses import fields
 
 import dateparser
 
@@ -18,6 +19,27 @@ ENTITIES: dict[str, type[Entity]] = {
     "task": Task,
     "note": Note
 }
+
+ENTITIES_COLUMNS = {
+    name: fields(cls)
+    for name, cls in ENTITIES.items()
+}
+
+FIELD_NAME_TO_ENTITY_NAME = {}
+
+for name, fields_ in ENTITIES_COLUMNS.items():
+    for field in fields_:
+        if not field.name in FIELD_NAME_TO_ENTITY_NAME.keys():
+            FIELD_NAME_TO_ENTITY_NAME.update({field.name: name})
+
+ALL_FIELDS = [
+    field for type in ENTITIES.values()
+    for field in fields(type)
+]
+
+ALL_FIELDS_NAME = [
+    field.name for field in ALL_FIELDS
+]
 
 
 def build_entity(**data):
