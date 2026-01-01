c7e377138cfda58c14cbe9d21866d32e958ddae0
Dvodnenko <thekovalskii@gmail.com>
2026-01-01 17:41:19 +0200

    method for casting complex filters

--- src/raw/daemon/domain/schemes.py
+++ src/raw/daemon/domain/schemes.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from typing import get_origin, Any
 
 from .entity import Entity
-from .utils import parse_datetime, parse_list
+from .utils import parse_datetime, parse_list, ENTITIES
 
 
 def cast(dict_: dict[str, str], cls: type[Entity]) -> dict[str, Any]:
@@ -26,5 +26,35 @@ def cast(dict_: dict[str, str], cls: type[Entity]) -> dict[str, Any]:
             result[key] = parse_list(value)
         else:
             result[key] = allowed[key].type(value)
+
+    return result
+
+def cast_filters(
+    filters: dict[str, dict[str, list[Any]]],
+):
+    for entity_name, filters_ in filters.items():
+        cls = ENTITIES[entity_name]
+        allowed = {f.name: f for f in fields(cls)}
+        result: dict[str, dict[str, list[Any]]] = {}
+        for filter_name_and_op, values_list in filters_.items():
+            if not result.get(entity_name):
+                result[entity_name] = {}
+
+            if "__" in filter_name_and_op:
+                filter_name = filter_name_and_op[
+                    0:filter_name_and_op.find("__")]
+            else:
+                filter_name = filter_name_and_op
+            
+            if allowed[filter_name].type is datetime:
+                result[entity_name][filter_name_and_op] = [parse_datetime(v) for v in values_list]
+            elif (
+                allowed[filter_name].type is list or
+                get_origin(allowed[filter_name].type) is list
+            ):
+                result[entity_name][filter_name_and_op] = [parse_list(v) for v in values_list]
+            else:
+                result[entity_name][filter_name_and_op] = [
+                    allowed[filter_name].type(v) for v in values_list]
 
     return result
