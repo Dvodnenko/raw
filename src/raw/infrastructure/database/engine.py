496e5d0d45d237d2f351749d6fda035fcf93964a
Dvodnenko <thekovalskii@gmail.com>
2026-01-12 15:20:52 +0200

    turn foreign keys on for each db connection
    it's needed for sqlite cascades

--- src/raw/infrastructure/database/engine.py
+++ src/raw/infrastructure/database/engine.py
@@ -4,3 +4,10 @@ from sqlalchemy import create_engine, event, Engine
 engine = create_engine(
     url="sqlite:///raw.db", # temporare url for tests
 )
+
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
