e76d2a47a34ad629e7eb4fa1f6f0f20e2dced42b
Dvodnenko <thekovalskii@gmail.com>
2026-01-12 15:25:30 +0200

    specification compiler implementation

---
+++ src/raw/infrastructure/adapters/spec_compiler.py
@@ -0,0 +1,45 @@
+from sqlalchemy import and_, or_, Table
+
+from ...domain import FieldSpec, And, Or, Not, SpecCompiler, Spec
+
+
+class SpecCompilerSA(SpecCompiler):
+    def compile(self, spec: Spec, table: Table):
+        if isinstance(spec, FieldSpec):
+            return self._compile_field(spec, table)
+        if isinstance(spec, And):
+            return and_(self.compile(spec, table) for spec in spec.items)
+        if isinstance(spec, Or):
+            return or_(self.compile(spec, table) for spec in spec.items)
+        if isinstance(spec, Not):
+            return ~self.compile(spec.spec)
+        raise TypeError(spec)
+
+    def _compile_field(self, spec: FieldSpec, table: Table):
+        try:
+            column = getattr(table.c, spec.field)
+        except AttributeError:
+            raise ValueError(f"Field not found: {spec.field}") from None
+        match spec.operator:
+            case "==":
+                return column == spec.value
+            case "!=":
+                return column != spec.value
+            case ">":
+                return column > spec.value
+            case ">=":
+                return column >= spec.value
+            case "<":
+                return column < spec.value
+            case "<=":
+                return column <= spec.value
+            case "like":
+                return column.like(spec.value)
+            case "notlike":
+                return column.notilike(spec.value)
+            case "in":
+                return column.in_(spec.value)
+            case "notin":
+                return column.notin_(spec.value)
+
+        raise ValueError(spec.operator)
