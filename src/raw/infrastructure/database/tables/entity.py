779cbeb6ac2327605f0cb5c66f2cc8b373a2003d
Dvodnenko <thekovalskii@gmail.com>
2026-01-20 20:20:44 +0200

    alter entity table from SA to raw SQL

--- src/raw/infrastructure/database/tables/entity.py
+++ src/raw/infrastructure/database/tables/entity.py
@@ -1,16 +1,12 @@
-from sqlalchemy import (
-    Table, Column, Integer, Text, String, ForeignKey)
+entity_table = """
+CREATE TABLE entity (
+    id INTEGER PRIMARY KEY,
+    type TEXT,
+    parent_id INTEGER NULLABLE,
+    title TEXT,
+    description TEXT,
+    icon TEXT,
 
-from ..metadata import metadata
-
-
-entity_table = Table(
-    "entity", metadata,
-    Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("type", String(50)),
-    Column("parent_id", Integer, 
-        ForeignKey("entity.id", ondelete="CASCADE"), nullable=True),
-    Column("title", String, nullable=False, unique=True),
-    Column("description", Text, nullable=True),
-    Column("icon", String, nullable=False, default=""),
+    FOREIGN KEY (parent_id) REFERENCES entity(id) ON DELETE CASCADE
 )
+"""
