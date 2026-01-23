6f7521c34b6a7fd7feb9cbd692c628a0cf605d46
Dvodnenko <thekovalskii@gmail.com>
2026-01-23 17:09:00 +0200

    add existential errors

--- src/raw/domain/exc.py
+++ src/raw/domain/exc.py
@@ -1,2 +1,33 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class EntityType(Enum):
+    TASK = "task"
+
+@dataclass(frozen=True)
+class EntityRef:
+    type: EntityType
+    id: int
+
+
 class DomainError(Exception):
     """Base domain error class"""
+
+
+## Existential Errors ##
+@dataclass(init=False)
+class AlreadyExistsError(DomainError):
+    entity: EntityRef
+
+    def __init__(self, entity: EntityRef):
+        self.entity = entity
+        super().__init__(f"{entity.type.value} already exists: {entity.id}")
+
+@dataclass(init=False)
+class NotFoundError(DomainError):
+    entity: EntityRef
+
+    def __init__(self, entity: EntityRef):
+        self.entity = entity
+        super().__init__(f"{entity.type.value} not found: {entity.id}")
