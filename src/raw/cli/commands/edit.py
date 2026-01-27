e1f6d8521a651798504fc0b92c691520b7f8a70b
Dvodnenko <thekovalskii@gmail.com>
2026-01-27 20:09:30 +0200

    edit cmd registry

---
+++ src/raw/cli/commands/edit.py
@@ -0,0 +1,53 @@
+import argparse
+
+from ...shared import MISSING
+from ..constants import EDITOR_SENTINEL
+from ..handlers.edit import handle_edit_cmd
+
+
+def register_edit_cmd(sub: argparse._SubParsersAction):
+    """
+    Registers "edit" CLI command and its options in provided subparser
+    """
+
+    parser = sub.add_parser("edit",
+        description="edit an entity",
+        help="edit an entity")
+    
+    parser.add_argument("id", type=int)
+
+    ## General Options, for All Entity Types
+    parser.add_argument("--title",
+        nargs="?",
+        const=EDITOR_SENTINEL,
+        default=MISSING,
+        metavar="<title>"
+    )
+    parser.add_argument("--description",
+        nargs="?",
+        const=EDITOR_SENTINEL,
+        default=MISSING,
+        metavar="<desc>"
+    )
+    parser.add_argument("--icon",
+        nargs="?",
+        const=EDITOR_SENTINEL,
+        default=MISSING,
+        metavar="<icon>"
+    )
+
+    ## Task Options
+    parser.add_argument("--status",
+        nargs="?",
+        const=EDITOR_SENTINEL,
+        default=MISSING,
+        metavar="<status>"
+    )
+    parser.add_argument("--deadline",
+        nargs="?",
+        const=EDITOR_SENTINEL,
+        default=MISSING,
+        metavar="<deadline>"
+    )
+
+    parser.set_defaults(handler=handle_edit_cmd)
