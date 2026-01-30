06326076fbdd5248f3bcc63d37153656e03866b7
Dvodnenko <thekovalskii@gmail.com>
2026-01-30 15:07:24 +0200

    add find cmd handler

---
+++ src/raw/cli/handlers/find.py
@@ -0,0 +1,28 @@
+import argparse
+
+from ...domain import InvalidValue
+from ...application import FindEntity, FindEntityQuery
+from ...infrastructure import UnitOfWorkSQL
+from ...config import DB_PATH
+from ..resolvers import resolve_arg
+from ..parsers import parse_infix
+
+
+def handle_find_cmd(args: argparse.Namespace):
+    raw_filter = resolve_arg("specification", args.where)
+
+    spec = None
+    if raw_filter:
+        try:
+            spec = parse_infix(raw_filter)
+        except Exception as exc:
+            raise InvalidValue("invalid filter") from exc
+
+    cmd = FindEntityQuery(
+        type=args.type.strip("s"),
+        spec=spec,
+    )
+    interactor = FindEntity(UnitOfWorkSQL(DB_PATH))
+
+    for entity in interactor.find(cmd):
+        print(f"#{entity.id} {entity.title}")
